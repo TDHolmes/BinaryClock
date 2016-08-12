554b2f6e0a8ebbc5a03b1d6aa58728d9044d7328
TDHolmes <tylerdanielholmes@gmail.com>
2016-08-12 13:48:08 -0700

    committing changes before doing dangerous macro to add docstrings

--- firmware/RTC_DS3231.c
+++ firmware/RTC_DS3231.c
@@ -1,7 +1,19 @@
 #include "RTC_DS3231.h"
 
 
+void RTC_init(rtc_time_t *t_ptr)
+{
+
+}
+
+
 void RTC_set_time(rtc_time_t *t_ptr, uint8_t hour, uint8_t minute, uint8_t second)
+{
+
+}
+
+
+void RTC_read_time(rtc_time_t *t_ptr)
 {
 
 }
