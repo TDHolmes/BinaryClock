424ae602f7586a503a97a0ae6dbfdeaa0d3f2657
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-06-11 15:59:25 -0700

    adding checks for successful returncodes

--- update_github_pages.sh
+++ update_github_pages.sh
@@ -1,28 +1,54 @@
 #/usr/bash
 echo "Updating github documentation!"
 
+function check_retcode() {
+    if [ $? -ne 0 ]
+    then
+        echo "Failure! Are all your folders correct and changes committed in git?"
+        exit -1
+    fi
+}
+
 printf "\n\n"
 echo "update documentation..."
 python documentation_build.py
+check_retcode
 
 printf "\n\n"
 echo "copy documentation assets out of the repo..."
 cd software/build/html
-cp -r ./ ../../../../sphinxDocumentation
+cp -r ./ ../../../../sphinxDocumentation > /dev/null
+check_retcode
 cd ../../..
 cd documents/doxygenDocumentation/html
-cp -r ./ ../../../../doxygenDocumentation
+cp -r ./ ../../../../doxygenDocumentation > /dev/null
+check_retcode
 cd ../../..
 
-printf "\n\nwaiting..."
-read
+printf "\n\n"
 echo "switch branches..."
-git checkout gh-pages
+git checkout gh-pages > /dev/null
+check_retcode
 
-printf "\n\nwaiting..."
-read
+printf "\n\n"
 echo "copy the built documentation into the gh-pages branch..."
 cd ..
-cp -r sphinxDocumentation BinaryClock/documents
-cp -r doxygenDocumentation BinaryClock/documents
+cp -r sphinxDocumentation BinaryClock/documents > /dev/null
+check_retcode
+cp -r doxygenDocumentation BinaryClock/documents > /dev/null
+check_retcode
 cd BinaryClock
+
+printf "\n\n"
+echo "Adding the changes to git..."
+git add . > /dev/null
+check_retcode
+git commit -m "[automated script] Updating documentation..." > /dev/null
+check_retcode
+
+printf "\n\n"
+echo "Jumping back to master..."
+git checkout master > /dev/null
+check_retcode
+
+echo "Done!"
