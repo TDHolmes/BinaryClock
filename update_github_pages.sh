db7c071135712d4cb8ad2c6d2e9e03992c3d5ce0
Tyler Holmes <tylerdanielholmes@gmail.com>
2017-06-11 15:23:23 -0700

    more verbose output of what's going on

--- update_github_pages.sh
+++ update_github_pages.sh
@@ -1,16 +1,20 @@
 #/usr/bash
-echo "Make sure you've run `python documentation_build.py` to make sure everything is up to date!"
+echo "Updating github documentation!"
 
-# update documentation
+printf "\n\n"
+echo "update documentation..."
 python documentation_build.py
 
-# copy documentation assets out of the repo
+printf "\n\n"
+echo "copy documentation assets out of the repo..."
 cp -r software/build/html ../sphinxDocumentation
 cp -r documents/doxygenDocumentation/html ../doxygenDocumentation
 
-# switch branches
+printf "\n\n"
+echo "switch branches..."
 git checkout gh-pages
 
-# copy the built documentation into the gh-pages branch
+printf "\n\n"
+echo "copy the built documentation into the gh-pages branch..."
 cp -r ../sphinxDocumentation documents
 cp -r ../doxygenDocumentation documents
