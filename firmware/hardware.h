3f9c6954292c75d785d3053800afe686268fe5ef
Tyler Holmes <tylerdanielholmes@gmail.com>
2018-02-05 22:40:17 -0800

    adding comment about fuse byte

--- firmware/hardware.h
+++ firmware/hardware.h
@@ -8,6 +8,13 @@
     \brief Hardware related definitions (e.g. oscillator speed).
 */
 
+/* IMPORTANT INFO: ATtiny fuse bit settings:
+ *   -U lfuse:w:0xe4:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+ *     lower fuse byte: 0xe4
+ *     upper fuse byte: 0xdf
+ *  extended fuse byte: 0xff
+ */
+
 #define ON  1 //!< defines the on state
 #define OFF 0 //!< defines the off state
 
